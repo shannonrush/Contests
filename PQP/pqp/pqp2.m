a5fcfd23cc1e14ec06395d4cb677dfaf36d701c9
Shannon Rush <shannonmrush@gmail.com>
2011-11-02 19:09:10 -0600

    breaking up training data to analyze word components

--- PQP/pqp/pqp2.m
+++ PQP/pqp/pqp2.m
@@ -3,11 +3,26 @@
 % analyzing "word" features only, combining all to make one common dictionary
 % for each example, goal is to predict P(Good|[array of words in all 3 word features]) using naive bayes
 
-% goodBag = csvread('goodBag.csv');
-% badBag = csvread('badBag.csv'); 
-% dictionary = csvread('dictionary.csv');
-% yInfo = csvread('yInfo.csv');
+data = csvread('training.csv');
+m = rows(data);
+wordData = cell(m,3);
 
-data = dlmread('subset.csv');
+fid = fopen('training.csv')
+for i=1:m
+	wordArray = [];
+	txt = fgetl(fid);
+	a = split(txt,',');
+	id = str2num(a(1,:));
+	result = str2num(a(10,:));
+	for row=7:9
+		b = split(txt(row,:),' ');
+		for brow=1:rows(b)
+			newRow = rows(wordArray)+1;
+			wordArray(newRow) = str2num(b(brow,:));
+		end
+	end
+	wordData(i,:)={id,wordArray,result};
+end
+
 
 
