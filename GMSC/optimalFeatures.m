6e951af04aaf3fd7208b07446b13da0ffcdf1316
Shannon Rush <shannonmrush@gmail.com>
2011-11-30 13:28:13 -0700

    optimal features for gmsc and general

--- GMSC/optimalFeatures.m
+++ GMSC/optimalFeatures.m
@@ -1,27 +1,24 @@
 function combinations = optimalFeatures(Xtrain,ytrain,Xcv,ycv,Xtest,ytest)
-	clear;
 	%OPTIMALFEATURES takes a matrix X and finds the error for different feature sets using cross validation and test data
 	% and returns the theta from the feature set with the lowest test error
 	
-	% features=columns(Xtrain);
-	features=5;
-	numRows = 0;
-	r=1;
-	for i=1:features
-		numRows = numRows + (2^(features-i));
-	end
-	combinations = zeros(numRows,features);
-	for feature=1:features
-		dims = (features-feature)+1;
-		for row=r:(2^(features-feature))
-			value = feature;
-			for dim=1:dims
-				combinations(row,dim) = value;
-				value++;
-			end
+	lambda=10;
+	numFeatures=columns(Xtrain);
+	elements = [1:numFeatures];
+	combinations = {};
+	cverror=[];
+	for i=2:numFeatures
+		results = unique(sort (perms (elements)(:,[1:i]), 2), 'rows');
+		for r=1:rows(results)
+			feature_set = results(r,:);
+			combinations(columns(combinations)+1) = feature_set;
+			theta = trainLinearReg([ones(rows(Xtrain), 1) Xtrain(:,feature_set)], ytrain, lambda);
+			cverror(columns(cverror)+1) = linearError(theta, Xcv(:,feature_set), ycv);
 		end
-		r = r + 2^(features-feature);
 	end
+	
+	
+	
 
 	
 end
