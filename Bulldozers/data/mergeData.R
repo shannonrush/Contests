dc91b380d0c191229bc73e904ab05ffa85149e14
Shannon Rush <shannonmrush@gmail.com>
2013-01-29 14:15:06 -0700

    fixed merge

--- Bulldozers/data/mergeData.R
+++ Bulldozers/data/mergeData.R
@@ -3,8 +3,8 @@ mergeData <- function() {
   ma <- read.csv("original/Machine_Appendix.csv")
   t <- read.csv("original/train.csv")
   v <- read.csv("original/valid.csv")
-  merged_train <- merge(ma,t,by="MachineID")
+  merged_train <- merge(ma,t,by=Reduce(intersect,list(names(ma),names(t))))
   write.csv(merged_train,"processed/merged_train.csv",row.names=F)
-  merged_valid <- merge(ma,v,by="MachineID")
+  merged_valid <- merge(ma,v,by=Reduce(intersect,list(names(ma),names(t))))
   write.csv(merged_valid,"processed/merged_valid.csv",row.names=F)
 }
--- Bulldozers/visualizations/visualizations.R
+++ Bulldozers/visualizations/visualizations.R
@@ -4,7 +4,7 @@ exploratoryData <- function () {
   # get just 10% of training data to more quickly explore data set
   library(caret)
   set.seed(100)
-  train <- read.csv("../data/original/train.csv")
+  train <- read.csv("../data/processed/training.csv")
   exploreIndex <- createDataPartition(train$SalePrice, list=FALSE, p = 0.1)
   write.csv(train[exploreIndex,],"explore.csv",row.names=F)
 }
@@ -21,20 +21,4 @@ salePriceByYearMade <- function() {
   plot(SalePrice ~ YearMade)
 }
 
-clustering <- function () {
-  data <- read.csv("../data/training.csv")
-  
-  hc <- hclust(dist(t(data[,1:ncol(data)])))
-  plot(hc)
-}
 
-
-# visualizations
-
-salePriceVsYearMade <- function() {
-  library("ggplot2")
-  data<-read.csv("../data/train.csv")
-  attach(data)
-  subdata<-data[YearMade!=1000,]
-  ggplot(subdata, aes(x=YearMade, y=SalePrice)) + geom_point()+scale_y_continuous(limit=c(4750, 142000))
-}
