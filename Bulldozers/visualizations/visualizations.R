20f51bb099d837a168aa4d157462976ee24b595f
Shannon Rush <shannonmrush@gmail.com>
2013-01-29 17:02:10 -0700

    adding some exploratory figures

--- Bulldozers/visualizations/visualizations.R
+++ Bulldozers/visualizations/visualizations.R
@@ -10,15 +10,34 @@ exploratoryData <- function () {
 }
 
 salePriceHistogram <- function() {
-  training <- read.csv("..data/training.csv")
+  training <- read.csv("../data/processed/training.csv")
   hist(training$SalePrice,xlab="Sale Price ($)", ylab="Bulldozers Purchased",main="Bulldozer Auction Sale Prices")
 }
 
 salePriceByYearMade <- function() {
-  data <- read.csv("explore.csv")
-  subdata <- data[data$YearMade!=1000,]
+  explore <- read.csv("explore.csv")
+  subdata <- explore[explore$YearMade!=1000,]
   attach(subdata)
   plot(SalePrice ~ YearMade)
+}
+
+salePriceByYearMadeMean <- function() {
+  explore <- read.csv("explore.csv")
+  sub <- explore[explore$YearMade!=1000,]
+  years<-unique(YearMade)
+  means<-sapply(sort(years),function(x)mean(sub[sub$YearMade==x,"SalePrice"]))
+  means_list <- as.list(means)
+  names(means_list)<-sort(years)
+  plot(names(means_list),sapply(names(means_list),function(x)means_list[x]),ylab="Mean Sale Price",xlab="Year Made",main="Mean Sale Price By Year")
+}
+
+salePriceByMonthSoldMean <- function() {
+  explore <- read.csv("explore.csv")
+  explore$monthSold <- apply(explore,1,function(x) {
+    browser()
+    date <- as.Date(x['saledate'])
+    as.numeric(format(date,format="%m"))
+    })
 }
 
 
