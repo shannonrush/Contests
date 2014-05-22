9fd82cbeb8a5dfaa217cbcd3a8f887246b00b28f
Shannon Rush <shannonmrush@gmail.com>
2014-05-22 11:14:12 -0600

    adding exploratory figures for Higgs boson

--- HiggsBoson/code/exploratory/exploratory.R
+++ HiggsBoson/code/exploratory/exploratory.R
@@ -1,5 +1,10 @@
-## read training data
+## read data
 train <- read.csv("../../data/original/training.csv")
+test <- read.csv("../../data/original/test.csv")
 
-## examine training data
+## examine data
+head(train, 1)
 str(train)
+summary(train)
+
+head(test, 1)
