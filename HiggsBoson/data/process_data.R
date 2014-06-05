4b930624a5c6ea1030f1e82b1829d4fa0a1261df
Shannon Rush <shannonmrush@gmail.com>
2014-06-05 12:56:17 -0600

    moving Higgs Boson into its own repo

--- HiggsBoson/data/process_data.R
+++
@@ -1,8 +0,0 @@
-## read data
-train <- read.csv("original/training.csv")
-
-## remove Weight variable - not allowed as classifying variable
-train$Weight <- NULL
-
-## create new CSV
-write.csv(train, file="processed/processed_train.csv", row.names=F)
